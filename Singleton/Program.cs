88ecffda0c788dd4a5e92697437091118a4a207c
Waseem Khalid <waseemsabra2@gmail.com>
2022-11-05 19:38:14 +0200

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Singleton
+{
+    private static Singleton _instance; //must be private and static prop
+
+    private Singleton() //must be private or protected constructor
+    {
+    }
+
+    public static Singleton Instance 
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+    }
+
+    public void DoSingletonOperation()//At least have one non-static public method for a singleton operation.
+    {
+        Console.WriteLine("singleton operation");
+    }
+}
